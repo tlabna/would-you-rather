dcf2ba973f7ebe3f10b3c1bc8dd84a275fb652f5
tlabna <tlabna@users.noreply.github.com>
2018-06-10 17:26:53 -0400

    feat: Add Navigation skeleton
    - Add Navigation component with styles
      - Not fully funtional just skeleton
      - isAuthed prop is defaulted to False
    - Add Navigation component to MainContainer

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
+import { navContainer, link, active } from './styles.css'
+
+Navigation.propTypes = NavLinks.propTypes = ActionLinks.propTypes = {
+  isAuthed: PropTypes.bool.isRequired,
+}
+
+Navigation.defaultProps = {
+  isAuthed: false,
+}
+
+function NavLinks({ isAuthed }) {
+  return isAuthed === true ? (
+    <ul>
+      <li>
+        <NavLink exact={true} className={link} activeClassName={active} to="/">
+          {'Home'}
+        </NavLink>
+      </li>
+    </ul>
+  ) : null
+}
+
+function ActionLinks({ isAuthed }) {
+  return isAuthed === true ? (
+    <ul>
+      <li>{'MODAL'}</li>
+      <li>
+        <NavLink className={link} to="/logout">
+          {'Logout'}
+        </NavLink>
+      </li>
+    </ul>
+  ) : (
+    <ul>
+      <li>
+        <NavLink exact={true} className={link} activeClassName={active} to="/">
+          {'Home'}
+        </NavLink>
+      </li>
+      <li>
+        <NavLink className={link} activeClassName={active} to="/auth">
+          {'Authenticate'}
+        </NavLink>
+      </li>
+    </ul>
+  )
+}
+
+export default function Navigation({ isAuthed }) {
+  return (
+    <div>
+      <nav className={navContainer}>
+        <NavLinks isAuthed={isAuthed} />
+        <ActionLinks isAuthed={isAuthed} />
+      </nav>
+    </div>
+  )
+}
--- src/containers/Main/MainContainer.js
+++ src/containers/Main/MainContainer.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Navigation } from 'components'
 import { container, innerContainer } from './styles.css'
 
 export default class MainContainer extends Component {
   static propTypes = {
     children: PropTypes.any.isRequired,
+    isAuthed: PropTypes.bool.isRequired,
   }
 
   render() {
     return (
       <div className={container}>
+        <Navigation isAuthed={this.props.isAuthed} />
         <div className={innerContainer}>{this.props.children}</div>
       </div>
     )
