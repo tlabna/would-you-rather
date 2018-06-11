5fa42235d4e0dd4927d4221ba5e41440a942219a
tlabna <tlabna@users.noreply.github.com>
2018-06-11 09:43:45 -0400

    feat: Add Results (presentational + container) component
    - /results will render Results component after auth

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Router } from 'react-router'
-import { MainContainer, HomeContainer, AuthenticateContainer } from 'containers'
+import {
+  MainContainer,
+  HomeContainer,
+  AuthenticateContainer,
+  ResultsContainer,
+} from 'containers'
 
 export default function getRoutes(checkAuth, history) {
   return (
@@ -10,6 +15,7 @@ export default function getRoutes(checkAuth, history) {
         <Switch>
           <Route exact={true} path="/" component={HomeContainer} />
           <Route path="/auth" component={AuthenticateContainer} />
+          <Route path="/results" component={ResultsContainer} />
         </Switch>
       </MainContainer>
     </Router>
---
+++ src/containers/Results/ResultsContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Results } from 'components'
+
+class ResultsContainer extends Component {
+  render() {
+    return <Results />
+  }
+}
+
+export default ResultsContainer
